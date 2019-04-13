36548ba5d9da16e884b5431a55a4ef7e8f29c651
Alexandre Pedrecal Campos <pedrecalc@gmail.com>
2019-04-13 19:09:41 -0300

    feat: added isFormatLegal func

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const inquirer = require('inquirer');
 const chalk = require('chalk');
 const figlet = require('figlet');
 const shell = require('shelljs');
+const Scry = require('scryfall-sdk');
+const Papa = require('papaparse');
+const fs = require('fs');
 
 const init = () => {
   console.log(
@@ -59,6 +62,45 @@ const askQuestions = () => {
   return inquirer.prompt(questions);
 };
 
+const isFormatLegal = async (file, header, format, filter) => {
+  const isPauperLegal = async cards => {
+    const cardPauperLegal = [];
+    for (let i = 0; i < cards.length; i++) {
+      const result = await Scry.Cards.byName(cards[i], true);
+      if (result.legalities.pauper === 'legal') {
+        cardPauperLegal.push({
+          cardName: result.name,
+          isPauperLegal: result.legalities.pauper,
+          colors: result.colors,
+        });
+      }
+    }
+    return cardPauperLegal;
+  };
+  const getCardsUniqName = async data => {
+    const namesArray = [];
+    let uniq = [];
+    for (let i = 1; i < data.length; i++) {
+      namesArray.push(data[i][0][0]);
+    }
+    uniq = [...new Set(namesArray)];
+    console.log(await isPauperLegal(uniq));
+  };
+  const readCSV = file => {
+    const cardsNames = [];
+    Papa.parse(file, {
+      step: result => {
+        cardsNames.push(result.data);
+      },
+      complete: results => {
+        const cards = getCardsUniqName(cardsNames);
+      },
+    });
+  };
+
+  readCSV(file);
+};
+
 const run = async () => {
   // show script introduction
   init();
@@ -66,7 +108,8 @@ const run = async () => {
   const answers = await askQuestions();
   const { FILENAME, HEADER, FORMAT, FILTER } = answers;
   // run the legalities validator
-
+  const file = fs.createReadStream(FILENAME);
+  isFormatLegal(file, 'Name', 'Pauper', 'both');
   // show success message
 };
 
